7d27d6268f02d2bd64087a09d0fc527d7871c106
chanzkie0407-dot <chanzkie0407@gmail.com>
2026-09-19 19:16:34 +0800

    Create auth-config.js

---
+++ shared/auth-config.js
@@ -0,0 +1,4 @@
+export const VALID_USERS = [
+  { id: 1, name: 'Admin', role: 'admin', password: '8888', storeId: 'ALL' },
+  { id: 2, name: 'Service Provider', role: 'service_provider', password: '19970407chan', storeId: 'ALL' }
+];
